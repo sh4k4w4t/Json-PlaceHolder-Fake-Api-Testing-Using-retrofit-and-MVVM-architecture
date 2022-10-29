81b60af7b7b736a40ac629607522bf6c4ffc029f
shakawat <shakawat_hossain@outlook.com>
2022-10-29 17:09:55 +0600

    Success 4

--- app/src/main/java/com/shakawat/jsonplaceholderapitestusingmvvm/View/Adapter/TodosAdapter.java
+++ app/src/main/java/com/shakawat/jsonplaceholderapitestusingmvvm/View/Adapter/TodosAdapter.java
@@ -2,9 +2,11 @@ package com.shakawat.jsonplaceholderapitestusingmvvm.View.Adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -37,6 +39,11 @@ public class TodosAdapter extends RecyclerView.Adapter<TodosAdapter.xViewHolder>
         holder.id.setText(todosModels.get(position).getId()+"");
         holder.title.setText(todosModels.get(position).getTitle()+"");
         holder.complete.setText(todosModels.get(position).getCompleted()+"");
+        if (todosModels.get(position).getId()%2==0){
+            holder.linearLayout.setBackgroundColor(Color.GREEN);
+        }else {
+            holder.linearLayout.setBackgroundColor(Color.MAGENTA);
+        }
     }
 
     @Override
@@ -46,6 +53,7 @@ public class TodosAdapter extends RecyclerView.Adapter<TodosAdapter.xViewHolder>
 
     protected static class xViewHolder extends RecyclerView.ViewHolder{
         TextView userId, id, title, complete;
+        LinearLayout linearLayout;
 
         public xViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -53,6 +61,7 @@ public class TodosAdapter extends RecyclerView.Adapter<TodosAdapter.xViewHolder>
             id= itemView.findViewById(R.id.idTodos);
             title= itemView.findViewById(R.id.titleTodos);
             complete= itemView.findViewById(R.id.completeTodos);
+            linearLayout=itemView.findViewById(R.id.todosLayout);
         }
     }
 }
